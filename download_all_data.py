2016619c5c926a9a25f7907995eb3aa0eb285531
Nguyễn Quốc Hiệu <121285650+nqh-tq-32052503@users.noreply.github.com>
2026-01-13 17:28:36 +0700

    Update: add list of HF datasets to reproduce

---
+++ download_all_data.py
@@ -0,0 +1,32 @@
+import os
+
+HF_TOKEN = ""
+# Kích hoạt hf_transfer để đạt tốc độ tối đa (viết bằng Rust)
+os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
+
+from huggingface_hub import snapshot_download
+
+hf_data = "./hf_data"
+if not os.path.exists(hf_data):
+    os.makedirs(hf_data)
+
+list_hf_datasets = [
+    "nguyenhieu3205xt/chest-x-data",
+    "nguyenhieu3205xt/cub-200-data",
+    "nguyenhieu3205xt/imagenet-r-data",
+    "nguyenhieu3205xt/tiny-imagenet-data",
+    "nguyenhieu3205xt/euro-sat-data",
+    "nguyenhieu3205xt/resisc-45-data",
+    "nguyenhieu3205xt/crop-disease-data"
+       
+]
+
+
+for dataset_id in list_hf_datasets:
+    print(f"Downloading {dataset_id}....")
+    snapshot_download(
+        repo_id=dataset_id, # nguyenhiext/your-dataset-name
+        repo_type="dataset", # hoặc "model"
+        local_dir=hf_data,
+        token=HF_TOKEN
+    )
